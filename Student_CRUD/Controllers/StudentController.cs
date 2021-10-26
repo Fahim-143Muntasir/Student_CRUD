cbfd97e2de652849f6a75fa3e0840ae17c9e9090
Farhad Hossain <61607180+Fahim-143Muntasir@users.noreply.github.com>
2021-10-26 09:51:33 +0600

    this is first commit

---
+++ Student_CRUD/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Student_CRUD.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student_CRUD.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        db_testEntities dbobj = new db_testEntities();
+        public ActionResult Student()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddStudent(Tbl_student model)
+        {
+            
+                Tbl_student obj = new Tbl_student();
+                obj.FirstName = model.FirstName;
+                obj.LastName = model.LastName;
+                obj.Email = model.Email;
+                obj.Mobile = obj.Mobile;
+                obj.Description = model.Description;
+                dbobj.Tbl_student.Add(obj);
+                dbobj.SaveChanges();               
+            
+            return View("Student");
+        }
+    }
+}
